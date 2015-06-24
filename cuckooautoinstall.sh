27de8ae78fdde81a6ee714d259cdaade3d2a6395
David Francos <dfrancos@buguroo.com>
2015-06-24 19:04:51 +0200

    - Full refactor - Got things sorted into functions - Per-distro package installation - Optional sudo - /etc/os-version distro identification (not really using it right now) - lsb_releas distro name identification - Improved version detection method for django - Forced mongodb (dropped the test) - Added a requirements.txt with python deps

--- cuckooautoinstall.sh
+++ cuckooautoinstall.sh
@@ -1,158 +1,124 @@
 #!/bin/bash
 
-SCRIPT_VERSION=0.1
+cat <<EO
+┌─────────────────────────────────────────────────────────┐
+│                CuckooAutoInstall 0.2                    │
+│ David Reguera García - Dreg <dreguera@buguroo.com>      │
+│ David Francos Cuartero - XayOn <dfrancos@buguroo.com>   │
+│            Buguroo Offensive Security - 2015            │
+└─────────────────────────────────────────────────────────┘
+EO
 
-echo "-"
-echo cuckooautoinstall Running Version: $SCRIPT_VERSION ....
-echo David Reguera Garcia aka Dreg - buguroo Offensive Security https://buguroo.com/ 
-echo "-"
-echo Contact: dreg@fr33project.org / dreguera@buguroo.com
-echo "-"
+source /etc/os-release
+source build_functions.bash
 
-set -x 
+SUDO="sudo"
+TMPDIR=$(mktemp -d)
+RELEASE=$(lsb_release -cs)
+ORIG_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
+VIRTUALBOX_REP="deb http://download.virtualbox.org/virtualbox/debian $RELEASE contrib"
 
-verlte() {
-    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+declare -a packages
+packages["debian"]="python-pip python-sqlalchemy mongodb python-bson python-dpkt python-jinja2 python-magic python-gridfs python-libvirt python-bottle python-pefile python-chardet git build-essential autoconf automake libtool dh-autoreconf libcurl4-gnutls-dev libmagic-dev python-dev tcpdump libcap2-bin virtualbox dkms python-pyrex"
+packages["ubuntu"]="python-pip python-sqlalchemy mongodb python-bson python-dpkt python-jinja2 python-magic python-gridfs python-libvirt python-bottle python-pefile python-chardet git build-essential autoconf automake libtool dh-autoreconf libcurl4-gnutls-dev libmagic-dev python-dev tcpdump libcap2-bin virtualbox dkms python-pyrex"
+
+[[ $UID != 0 ]] && {
+    type -f $SUDO || {
+        echo "You're not root and you don't have $SUDO, please become root or install $SUDO before executing $0"
+        exit
+    }
+} || {
+    $SUDO=""
 }
 
-ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+[[ ! -e /etc/debian_version ]] && {
+    echo "This script currently works only on debian-based (debian, ubuntu...) distros"
+    exit 1
+}
 
-if [ -f /etc/lsb-release ]; then
-    . /etc/lsb-release
-    OS=`echo $DISTRIB_ID | tr '[:upper:]' '[:lower:]'`
-    VER=`echo $DISTRIB_RELEASE | tr '[:upper:]' '[:lower:]'`
-elif [ -f /etc/debian_version ]; then
-    OS=debian
-    VER=$(cat /etc/debian_version | tr '[:upper:]' '[:lower:]')
-elif [ -f /etc/redhat-release ]; then
-    # TODO
-    ...
-else
-    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-    VER=$(uname -r | tr '[:upper:]' '[:lower:]')
-fi
-
-CODENAME=`cat /etc/*-release | grep "VERSION=" | tr '[:upper:]' '[:lower:]'`
-CODENAME=${CODENAME##*\(}
-CODENAME=${CODENAME%%\)*}
-
-echo $OS $VER $ARCH $CODENAME
-    
-if [ "debian" = $OS ];
-then
-    echo Debian...
-    
-    if ! hash sudo 2>/dev/null; then
-        echo install sudo from the root account: apt-get install sudo
-        exit
-    fi
-
-    sudo apt-get -y install python-pip python-sqlalchemy mongodb python-bson python-dpkt python-jinja2 python-magic python-gridfs python-libvirt python-bottle python-pefile python-chardet
-    sudo pip install django
-    sudo pip install pymongo -U
-    sudo apt-get -y install git
-    sudo apt-get -y install build-essential
-    sudo apt-get -y install autoconf automake libtool
-
-    #jansson
-    sudo apt-get install dh-autoreconf
+clone_repos(){
     git clone https://github.com/akheron/jansson
-    cd jansson
+    git clone https://github.com/plusvic/yara
+}
+
+clone_cuckoo(){
+    cd /home/cuckoo/
+    $SUDO git clone https://github.com/cuckoobox/cuckoo
+    $SUDO chown -R cuckoo:cuckoo cuckoo
+    cd $TMPDIR
+}
+
+create_hostonly_iface(){
+    $SUDO vboxmanage hostonlyif create
+    $SUDO iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
+    $SUDO iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+    $SUDO iptables -A POSTROUTING -t nat -j MASQUERADE
+    $SUDO sysctl -w net.ipv4.ip_forward=1
+}
+
+create_cuckoo_user(){
+    $SUDO adduser  --disabled-password -gecos "" cuckoo
+    $SUDO usermod -G vboxusers cuckoo
+}
+
+setcap(){
+    $SUDO setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+}
+
+fix_django_version(){
+    cd /home/cuckoo/
+    python -c "import django; from distutils.version import LooseVersion; import sys; sys.exit(LooseVersion(django.get_version()) <= LooseVersion('1.5'))" && { 
+        egrep -i "templates = \(.*\)" cuckoo/web/web/settings.py || sed -i '/TEMPLATE_DIRS/{ N; s/.*/TEMPLATE_DIRS = \( \("templates"\),/; }' cuckoo/web/web/settings.py
+    }
+    cd $TMPDIR
+}
+
+enable_mongodb(){
+    cd /home/cuckoo/
+    sed -i '/\[mongodb\]/{ N; s/.*/\[mongodb\]\nenabled = yes/; }' cuckoo/conf/reporting.conf
+    cd $TMPDIR
+}
+
+build_jansson(){
+    # Not cool =(
+    cd ${TMPDIR}/jansson
     autoreconf -vi
     ./configure
     make
     make check
-    sudo make install
-    sudo autoreconf -i --force
-    cd ..
-    #jansson end
+    $SUDO make install
+    $SUDO autoreconf -i --force
+    cd ${TMPDIR}
+}
 
-    sudo apt-get -y install libcurl4-gnutls-dev libmagic-dev python-dev
-
-    #yara
-    git clone https://github.com/plusvic/yara
-    cd yara
+build_yara(){
+    cd ${TMPDIR}/yara
     ./bootstrap.sh
-    sudo autoreconf -i --force
+    $SUDO autoreconf -i --force
     ./configure --enable-cuckoo --enable-magic
     make
-    make install
+    $SUDO make install
     cd yara-python/
-    python setup.py build
-    sudo python setup.py install
-    cd ..
-    cd ..
-    #yara end
+    $SUDO python setup.py install
+    cd ${TMPDIR}
+}
 
-    #ssdeep
-    wget -c -L http://sourceforge.net/projects/ssdeep/files/latest/download?source=files -O ssdeep.tar.gz
-    tar -zxvf ssdeep.tar.gz
-    cd `tar tzf ssdeep.tar.gz | sed -e 's@/.*@@' | uniq`
-    ./configure
-    make
-    make check
-    sudo make install
-    sudo ldconfig
-    cd ..
-    #ssdeep end
 
-    #pydeep
-    sudo apt-get install unzip
-    wget -c https://github.com/kbandla/pydeep/archive/master.zip
-    unzip -o master.zip
-    cd pydeep-master
-    python setup.py build
-    sudo python setup.py install
-    cd ..
-    #pydeep end
 
-    #virtualbox
-    VIRTUALBOX_REP="deb http://download.virtualbox.org/virtualbox/debian $CODENAME contrib"
-    if ! grep -q "$VIRTUALBOX_REP" /etc/apt/sources.list; then
-        echo $VIRTUALBOX_REP >> /etc/apt/sources.list
-    fi
-    wget -c https://www.virtualbox.org/download/oracle_vbox.asc
-    sudo apt-key add oracle_vbox.asc
-    sudo apt-get update
-    sudo apt-get -y install virtualbox
-    sudo apt-get -y install dkms
+cd ${TMPDIR}
+echo ${VIRTUALBOX_REP} |$SUDO tee /etc/apt/sources.list.d/virtualbox.list
+wget -O - https://www.virtualbox.org/download/oracle_vbox.asc | $SUDO apt-key add -
+$SUDO apt-get update
+$SUDO apt-get install -y  ${packages["${RELEASE}"]}
+$SUDO apt-get -y install 
+$SUDO pip install -r ${ORIG_DIR}/requirements.txt
 
-    sudo vboxmanage hostonlyif create
-    sudo iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
-    sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-    sudo iptables -A POSTROUTING -t nat -j MASQUERADE
-    sudo sysctl -w net.ipv4.ip_forward=1
-    sudo adduser  --disabled-password -gecos "" cuckoo
-    sudo usermod -G vboxusers cuckoo
-    #virtualbox end
-
-    #tcpdump
-    sudo apt-get -y install tcpdump
-    sudo apt-get -y install libcap2-bin 
-    sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-    #tcpdump end
-
-    #cuckoo
-    git clone git://github.com/cuckoobox/cuckoo.git
-    #cuckoo end
-
-    #django fix
-    DJANGO_VERSION=`python -c "import django; print(django.get_version())"`
-    verlte 1.5 $DJANGO_VERSION && FIX_DJANGO=false || FIX_DJANGO=true
-    if [ "$FIX_DJANGO" = true ]; then
-        cat cuckoo/web/web/settings.py | grep -A1 "TEMPLATE_DIRS = (" | cut -d# -f2 | grep "templates" | grep "(" | grep ")"
-        if [ $? -ne 0 ]; then
-            echo "Fixing settings.py...."
-            sed -i '/TEMPLATE_DIRS/{ N; s/.*/TEMPLATE_DIRS = \( \("templates"\),/; }' cuckoo/web/web/settings.py
-        fi
-    fi
-    #django fix end
-
-    #enable mongodb
-    cat cuckoo/conf/reporting.conf | grep -A1 "\[mongodb\]" | cut -d# -f2 | grep "enabled = no" 
-    if [ $? -eq 0 ]; then
-        echo "Enabling mongodb in reporting.conf"
-        sed -i '/\[mongodb\]/{ N; s/.*/\[mongodb\]\nenabled = yes/; }' cuckoo/conf/reporting.conf
-    fi
-    #enable mongodb end
-fi
+clone_repos
+clone_cuckoo
+build_jansson
+build_yara
+create_hostonly_iface
+create_cuckoo_user
+setcap
+fix_django_version
+enable_mongodb
